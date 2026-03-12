4d91a2e5b0c5b80dafd307255694d5794855d406
angelmon091 <panchpier09@gmail.com>
2026-03-11 23:13:54 -0600

    MobileApplication"
    git push origin master

---
+++ app/src/main/java/com/example/proyectofinal/MainActivity.kt
@@ -0,0 +1,99 @@
+package com.example.proyectofinal
+
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.proyectofinal.databinding.ActivityMainBinding
+import com.google.android.material.navigation.NavigationView
+
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.mainContent) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            binding.appBarLayout.setPadding(0, systemBars.top, 0, 0)
+            v.setPadding(systemBars.left, 0, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+        
+        binding.toolbar.setNavigationOnClickListener {
+            binding.drawerLayout.openDrawer(GravityCompat.START)
+        }
+
+        binding.navView.setNavigationItemSelectedListener(this)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    // AQUÍ CONFIGURAS LAS ACCIONES DEL MENÚ LATERAL
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.nav_inicio -> {
+                Toast.makeText(this, "Inicio seleccionado", Toast.LENGTH_SHORT).show()
+            }
+            R.id.nav_recordatorio -> {
+                // code
+            }
+            R.id.nav_favoritos -> {
+                // Tcode
+            }
+            R.id.nav_escuela -> {
+                // code
+            }
+            R.id.nav_proyecto -> {
+                // Tcode
+            }
+            R.id.nav_trabajo -> {
+                // code
+            }
+            R.id.nav_ajustes -> {
+                // code
+            }
+            R.id.nav_cerrar_sesion -> {
+                // code
+                finish()
+            }
+        }
+        
+        // Esto cierra el menú después de tocar una opción
+        binding.drawerLayout.closeDrawer(GravityCompat.START)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_search -> true
+            R.id.action_more -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun onBackPressed() {
+        if (binding.drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            binding.drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            @Suppress("DEPRECATION")
+            super.onBackPressed()
+        }
+    }
+}
